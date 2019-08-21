9d71fd40b43f3cdcafed34ad7145a92cbc551b35
Varun Sonava <varunsonava@gmail.com>
2019-08-21 20:16:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/iot_led/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.iot_led;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashActivity.this,MainActivity.class));
+        finish();
+    }
+}
